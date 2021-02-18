416bb1573127819794b77ae192b9dce935f2c9f1
GuimaraesSl <aazzaazzaabb@gmail.com>
2021-02-17 21:19:01 -0300

    first commit

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+import 'package:projeto_igreja/views/sign_In/sing_in_view.dart';
+import 'package:projeto_igreja/views/sign_In/user_form_view.dart';
+import 'package:projeto_igreja/views/user_list_view.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  UserListView.routeName: (context) => UserListView(),
+  UserFormView.routeName: (context) => UserFormView(),
+  SignInView.routeName: (context) => SignInView(),
+};
